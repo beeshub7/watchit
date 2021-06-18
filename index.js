dfa8d294435a7aef2d4917f6124581622a04ec06
Shubham Khandelwal <beeshub7@gmail.com>
2021-06-18 11:29:57 +0530

    Write chokidar event handlers

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 #!/usr/bin/env node
+
+const chokidar = require('chokidar');
+const debounce = require('lodash.debounce');
+
+const start = debounce(() => {
+    console.log('START USERS PROGRAM');
+}, 100);
+
+chokidar
+    .watch('.')
+    .on('add', start)
+    .on('change', () => console.log('FILE CHANGED!'))
+    .on('unlink', () => console.log('FILE UNLINKED!'));
