052d9b532340e6ec9aa9db2a3b9ed982d7b7ab07
Shubham Khandelwal <beeshub7@gmail.com>
2021-06-20 15:53:48 +0530

    Add caporal to start the action with optional filename argument

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
+const program = require('caporal');
 
-const start = debounce(() => {
-    console.log('START USERS PROGRAM');
-}, 100);
+program
+    .version('0.0.1')
+    .argument('[filename]', 'Name of a file to execute')
+    .action(async ({filename}) => {
+        const name = filename || 'index.js';
 
-chokidar
-    .watch('.')
-    .on('add', start)
-    .on('change', () => console.log('FILE CHANGED!'))
-    .on('unlink', () => console.log('FILE UNLINKED!'));
+        try {
+            await fs.promises.access(name);
+        } catch (e) {
+            throw new Error(`Couldn't find the file ${filename}`);
+        }
+
+        const start = debounce(() => {
+            console.log('START USERS PROGRAM');
+        }, 100);
+
+        chokidar
+            .watch('.')
+            .on('add', start)
+            .on('change', start)
+            .on('unlink', start);
+    });
+
+program.parse(process.argv);
